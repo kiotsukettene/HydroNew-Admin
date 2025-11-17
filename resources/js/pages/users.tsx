65f3727bf07ad760be6bb349bf3ace5387891552
Cel3stt <mariannecelest@gmail.com>
2025-11-18 01:06:06 +0800

    Add Users page with breadcrumbs and layout

---
+++ resources/js/pages/users.tsx
@@ -0,0 +1,24 @@
+import AppLayout from '@/layouts/app-layout'
+import users from '@/routes/users';
+import { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react'
+import React from 'react'
+
+export default function Users() {
+
+    const breadcrumbs: BreadcrumbItem[] = [
+        {
+            title: 'Users',
+            href: users.index().url,
+        },
+    ];
+
+  return (
+     <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Users" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+
+            </div>
+        </AppLayout>
+  )
+}
