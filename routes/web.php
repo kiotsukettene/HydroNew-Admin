7c290301ba5e9a044215d72e71f06d3c82b3f41e
kiotsukettene <kiotsuketteneloreto@gmail.com>
2026-01-26 01:09:28 +0800

    Added routes to analytics page

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard/sensor-systems', [DashboardController::class, 'getSensorSystems'])->name('dashboard.sensor-systems');
+    Route::get('dashboard/ph-tds-readings', [DashboardController::class, 'getPhTdsReadings'])->name('dashboard.ph-tds-readings');
 
     Route::get('users/archived', [UserController::class, 'archived'])->name('users.archived');
     Route::patch('users/{id}/archive', [UserController::class, 'archive'])->name('users.archive');
     Route::patch('users/{id}/unarchive', [UserController::class, 'unarchive'])->name('users.unarchive');
     Route::resource('users', UserController::class);
 
+    Route::get('analytics/api/users-devices', [AnalyticsController::class, 'getUsersDevices'])->name('analytics.api.users-devices');
+    Route::get('analytics/api/crops-harvest', [AnalyticsController::class, 'getCropsHarvest'])->name('analytics.api.crops-harvest');
+    Route::get('analytics/api/yields', [AnalyticsController::class, 'getYields'])->name('analytics.api.yields');
+    Route::get('analytics/api/water-treatment', [AnalyticsController::class, 'getWaterTreatment'])->name('analytics.api.water-treatment');
     Route::resource('analytics', \App\Http\Controllers\Analytics\AnalyticsController::class);
 
     Route::get('devices/archived', [DeviceController::class, 'archived'])->name('devices.archived');
